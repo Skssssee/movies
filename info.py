c0507055a789f11d046d60ae4f0cefacfafa7b5e
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-04-04 15:10:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', 'd643430b289378849c13f2bdccb2de55')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7784747674:AAH42ej-oL06u2fALyiuMrhwbrtjFPJfthY')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7613349267').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7613349267 1249672673').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/cinexhubs") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002295313217'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/cinexhub_2')
@@ -36,10 +36,10 @@ BIN_CHANNEL = int(environ.get('BIN_CHANNEL','0'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002524071231'))
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
 auth_channel = environ.get('AUTH_CHANNEL', '')
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002687491032'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002371020945')
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002362397427'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cinexhub') #Support group link ( make sure bot is admin )
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cinexhub_support') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', False)
